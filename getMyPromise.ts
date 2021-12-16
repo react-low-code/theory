f5b9a7ae8091b34f8d284bd1ca0ac19b9f9119d9
秦小倩 <1395294694@qq.com>
2021-12-16 21:15:53 +0800

    feat: getMyPromise

---
+++ getMyPromise.ts
@@ -0,0 +1,22 @@
+function getMyPromise(p, finish = false) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (finish) {
+            resolve(p)
+            } else {
+            reject(p)
+            }
+        }, 1000)
+    })
+}
+
+const myPromise1 = getMyPromise('myPromise1的状态会变成 rejected', false)
+// 注意：在这里我将 myPromise1 传给了 p
+const myPromise2 = getMyPromise(myPromise1, true)
+
+myPromise2.then((val) => {
+    console.log(`我被兑现了，并且兑现的值是: ${val}`)
+}).catch((error) => {
+    console.log(`我被拒绝了, 理由是: "${error}"`)
+})
+
