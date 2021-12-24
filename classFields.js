ed01763d9736773694d6a84985d7dc3014983505
秦小倩 <1395294694@qq.com>
2021-12-24 18:52:25 +0800

    字段访问修饰符

--- classFields.js
+++ classFields.js
@@ -1,30 +1,30 @@
 class HelloFields {
     static allInstance = []
-    static appendInstance(item) {
+    static #appendInstance(item) {
         // 静态方法可以用 this 关键字，调用同一个类中的其他静态字段
         this.allInstance.push(item) // lineA
     }
 
     field = ''
-    lastName = ''
-    firstName = ''
+    #lastName = ''
+    #firstName = ''
 
     constructor(field) {
         this.field = field
         // 在构造函数和实例方法中使用 this.constructor 访问静态字段
-        this.constructor.appendInstance(this) // linB
+        this.constructor.#appendInstance(this) // linB
     }
 
-    changeField(field) {
+    changeField = (field) => {
         this.field = field
     }
 
     get fullName() {
-        return this.firstName + ' ' + this.lastName
+        return this.#firstName + ' ' + this.#lastName
     }
 
     set fullName(name) {
-        [this.firstName, this.lastName] = name.split(" ")
+        [this.#firstName, this.#lastName] = name.split(" ")
     }
 }
 
